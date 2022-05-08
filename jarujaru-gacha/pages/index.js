65c23d73c391f600a2e25c97a48b8f98f1a3a1a7
RyumaRyama <ryumaryama@gmail.com>
2022-05-09 01:39:10 +0900

    奴ボタン設置

--- jarujaru-gacha/pages/index.js
+++ jarujaru-gacha/pages/index.js
@@ -1,11 +1,16 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
       <h1 className={styles.title}>ジャルジャルの奴ガチャ作る奴</h1>
-      <div className={styles.video}>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/NE7uHTxUJgM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      <div className={styles.videoContainer}>
+        <div className={styles.video}>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/NE7uHTxUJgM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.yatsuButton} type='button'>奴</button>
       </div>
     </>
   )
