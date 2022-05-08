77a6e812684ea4c2645fa27d4e24576e3e51dc3e
RyumaRyama <ryumaryama@gmail.com>
2022-05-09 00:57:30 +0900

    テスト用動画の埋め込み

--- jarujaru-gacha/pages/index.js
+++ jarujaru-gacha/pages/index.js
@@ -2,6 +2,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>ジャルジャルの奴ガチャ作る奴</h1>
+    <>
+      <h1 className={styles.title}>ジャルジャルの奴ガチャ作る奴</h1>
+      <div className={styles.video}>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/NE7uHTxUJgM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      </div>
+    </>
   )
 }
